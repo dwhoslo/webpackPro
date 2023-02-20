9af99e97af4d4f24536854eed9592e309419fc49
DiavenyHoo <468133459@qq.com>
2023-02-20 22:49:49 +0800

    修改声明文件

--- shimd.d.ts
+++ shimd.d.ts
@@ -1,9 +1,9 @@
 // 文件路径 shims.d.ts
 declare module '*.vue' {
-    import type { DefineComponent } from 'vue'
-    const component: DefineComponent<{}, {}, any>
-    export default component
-  }
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
    
   // 对vue进行类型补充说明
 //   declare module '@vue/runtime-core' {
